70c1d949e69957b7f7dd4f796175981b8be9f14d
unknown <manueleforero@gmail.com>
2020-07-14 18:38:22 -0500

    aca termine switchstile

--- lib/src/pages/slider_page.dart
+++ lib/src/pages/slider_page.dart
@@ -9,39 +9,51 @@ class Sliderp extends StatefulWidget {
 
 class _SliderpState extends State<Sliderp> {
   double _valor = 100;
+  bool _micheck = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Sliders...')),
-      body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Slider(
-              label: 'Tamaño de la imagen',
-              value: _valor,
-              min: 50,
-              max: 400,
-              divisions: 100,
-              onChanged: (valor) {
-                setState(() {
-                  _valor = valor;
-                  print(valor.toInt());
-                });
-              },
-            ),
-            Text(_valor.toInt().toString()),
-            AnimatedContainer(
-              duration: Duration(milliseconds: 1),
-              child: Image(
-                image: NetworkImage(
-                    'https://images.pexels.com/photos/814499/pexels-photo-814499.jpeg'),
-                width: _valor,
-                height: _valor,
-                fit: BoxFit.contain,
-              ),
-            ),
-          ]),
+      body:
+          Column(mainAxisAlignment: MainAxisAlignment.start, children: <Widget>[
+        SizedBox(height: 40),
+        Slider(
+          label: 'Tamaño de la imagen',
+          value: _valor,
+          min: 50,
+          max: 350,
+          divisions: 100,
+          onChanged: (!_micheck)
+              ? null
+              : (valor) {
+                  setState(() {
+                    _valor = valor;
+                    print(valor.toInt());
+                  });
+                },
+        ),
+        Text(_valor.toInt().toString()),
+        SwitchListTile(
+          value: _micheck,
+          title: Text('Bloquear Slider'),
+          onChanged: (valor) {
+            setState(() {
+              _micheck = valor;
+            });
+          },
+        ),
+        AnimatedContainer(
+          duration: Duration(milliseconds: 1),
+          child: Image(
+            image: NetworkImage(
+                'https://images.pexels.com/photos/814499/pexels-photo-814499.jpeg'),
+            width: _valor,
+            height: _valor,
+            fit: BoxFit.contain,
+          ),
+        ),
+      ]),
     );
   }
 }
